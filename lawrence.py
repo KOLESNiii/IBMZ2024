73ea2955cc9f1ac4c0f419cb4f4f58926ac357c1
09lkei <09lkei@brightoncollege.net>
2024-10-19 16:55:59 +0100

    fix typo

--- lawrence.py
+++ lawrence.py
@@ -6,7 +6,7 @@ def imageToBundle(image):
     img = opencv.imread(image)
     year = image[0:4],
     coords = ''.join(list(filter(lambda a: str.isdigit(a),image[7:])))
-    return imageBundle(img coords, year)
+    return imageBundle(img, coords, year)
 
 def batchImages(folder):
     files = os.listdir(folder)
