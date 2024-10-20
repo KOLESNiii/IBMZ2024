bb57537e6987759a2bc57709f8248c33b20ea5c0
lpm124 <lpm124@ic.ac.uk>
2024-10-20 02:50:51 +0100

    Done

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import imageBundle
 import segmentsplit
 import classification
 import timk
+import analysis
 
 if __name__ == "__main__":
-    for img in batchImages("images"):
+    for (img16, img24) in zip(batchImages('images2016'), batchImages('images2024')):
+        biome16 = classification.classify_image(img16)
+        biome24 = classification.classify_image(img24)
         
-
+        analysis.compare(img16, img24)
